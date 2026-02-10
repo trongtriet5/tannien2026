cd1885c0194d7be9619496f18aa68be159c42678
trongtriet5 <job.nguyentrongtriet@gmail.com>
2026-02-10 20:03:33 +0700

    update image size of hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,16 +1,12 @@
 export default function HeroSection() {
   return (
-    <section className="relative w-full h-[60vh] md:h-[70vh] overflow-hidden" style={{ backgroundColor: '#9D0000' }}>
+    <section className="relative w-full h-[60vh] md:h-[70vh] overflow-hidden">
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="/hero_section.png"
+        src="/hero_section_1.png"
         alt="Tân Niên 2026"
-        className="absolute inset-0 w-full h-full object-contain object-center"
+        className="absolute inset-0 w-full h-full object-cover object-center"
       />
-      {/* Left fade */}
-      <div className="absolute inset-y-0 left-0 w-16 md:w-24" style={{ background: 'linear-gradient(to right, #9D0000, transparent)' }} />
-      {/* Right fade */}
-      <div className="absolute inset-y-0 right-0 w-16 md:w-24" style={{ background: 'linear-gradient(to left, #9D0000, transparent)' }} />
     </section>
   );
 }
