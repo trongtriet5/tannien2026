49ee299cab7e2f51d9d877c4b46ef3be122b4d71
Nguyen Trong Triet <103195920+trongtriet5@users.noreply.github.com>
2026-02-10 21:55:13 +0700

    Update hero section image source

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,7 +3,7 @@ export default function HeroSection() {
     <section className="relative w-full h-[60vh] md:h-[70vh] overflow-hidden">
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="/hero_section_1.png"
+        src="/hero_section.png"
         alt="Tân Niên 2026"
         className="absolute inset-0 w-full h-full object-cover object-center"
       />
